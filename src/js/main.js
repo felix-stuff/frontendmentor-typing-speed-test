5d96296c7c7e7d92b4d85209c904a5d8fcfec14a
Felix Kaiser <138372845+felix-stuff@users.noreply.github.com>
2026-01-02 12:18:32 +0100

    Add end game functionality

--- src/js/main.js
+++ src/js/main.js
@@ -14,24 +14,42 @@ const game = {
     },
   },
   selectedMode: 'timed',
-  difficulty: 'medium',
+  difficulty: 'hard',
   time: 0,
   passage: '',
+  words: 0,
   wordsPerMinute: 0,
   results: [],
+  highScore: 0,
+  typed: 0,
+  currentChar: 0,
 };
 
 // DOM selectors
 const userInput = document.getElementById('user-input');
-const output = document.getElementById('output');
 const passage = document.getElementById('passage');
-const startButton = document.getElementById('startButton');
-const resetButton = document.getElementById('resetButton');
+const startButton = document.getElementById('start-button');
+const startButtonContainer = document.getElementById('start-button-container');
+const resetButton = document.getElementById('reset-button');
+const resetButtonContainer = document.getElementById('reset-button-container');
+const restartButton = document.getElementById('restart-button');
 const difficulty = document.getElementById('difficulty');
 const wpm = document.getElementById('words-per-minute');
+const finalWpm = document.getElementById('final-words-per-minute');
 const mode = document.getElementById('mode');
 const time = document.getElementById('time');
 const accuracy = document.getElementById('accuracy');
+const finalAccuracy = document.getElementById('final-accuracy');
+const highScore = document.getElementById('high-score');
+const testComplete = document.getElementById('test-complete');
+const finalCorrect = document.getElementById('final-correct');
+const finalErrors = document.getElementById('final-errors');
+const gameContainer = document.getElementById('game-container');
+
+// global variables
+const blockedKeys = ['Backspace', 'ArrowLeft', 'ArrowRight', 'Delete'];
+let timerId = null;
+const timeOut = 120;
 
 // fetch passge async
 const loadPassage = async (difficulty) => {
@@ -59,31 +77,35 @@ const populateDom = (text) => {
 const startGame = () => {
   userInput.value = '';
   userInput.focus();
-  output.textContent = '';
   clearInterval(timerId);
   game.errorCounter = 0;
   startTimer(game.mode[game.selectedMode].direction, game.mode[game.selectedMode].startTime);
-  resetButton.classList.remove('hidden');
-  startButton.classList.add('hidden');
+  resetButtonContainer.classList.remove('hidden');
+  startButtonContainer.classList.add('hidden');
+  userInput.addEventListener('input', handleInput);
+  game.wordsPerMinute = 0;
+  passage.children[game.currentChar].classList.add('bg-white', 'opacity-20');
+  passage.classList.remove('blur-[6px]', 'opacity-40');
 };
 
 const resetGame = () => {
   userInput.value = '';
-  output.textContent = '';
   clearInterval(timerId);
   game.errorCounter = 0;
   time.innerHTML = `0:${String(game.mode[game.selectedMode].startTime).padStart(2, '0')}`;
   game.wordsPerMinute = 0;
-  resetButton.classList.add('hidden');
-  startButton.classList.remove('hidden');
+  resetButtonContainer.classList.add('hidden');
+  startButtonContainer.classList.remove('hidden');
   game.results = [];
   game.accuracy = 100;
   accuracy.textContent = '100%';
   populateDom(game.passage);
+  testComplete.classList.add('hidden');
+  gameContainer.classList.remove('hidden');
+  game.wordsPerMinute = 0;
+  passage.classList.add('blur-[6px]', 'opacity-40');
+  loadPassage(game.difficulty);
 };
-
-let timerId = null;
-let timeOut = 120;
 
 const startTimer = (direction, startTime) => {
   game.time = startTime;
@@ -92,8 +114,14 @@ const startTimer = (direction, startTime) => {
     game.time += direction;
 
     // stop timer depending on game mode
-    if (direction > 0 && game.time >= timeOut) clearInterval(timerId);
-    if (direction < 0 && game.time <= 0) clearInterval(timerId);
+    if (direction > 0 && game.time >= timeOut) {
+      clearInterval(timerId);
+      endGame();
+    }
+    if (direction < 0 && game.time <= 0) {
+      clearInterval(timerId);
+      endGame();
+    }
 
     if (game.time > 60) {
       let minutes = Math.floor(game.time / 60);
@@ -109,45 +137,121 @@ const startTimer = (direction, startTime) => {
 startButton.addEventListener('click', startGame);
 passage.addEventListener('click', startGame);
 
-userInput.addEventListener('input', handleInput);
+userInput.addEventListener('keydown', handleKeydown);
 
 function handleInput(e) {
   const typed = e.target.value;
 
-  output.textContent = typed;
-
   updateResults(typed);
   updateAccuracy();
   updateHighlighting();
+
+  if (typed.split(' ')) {
+    game.words = typed.split(' ').length - 1;
+  }
+
+  calculateWordsPerMinute();
+
+  // end game if all chars are correct
+  const correctChars = game.results.filter((result) => result === 'correct').length;
+  if (correctChars === game.passage.length) {
+    endGame();
+  }
 }
 
 function updateResults(typed) {
   for (let i = 0; i < game.passage.length; i++) {
-    if (typed[i] === undefined) game.results[i] = 'pending';
-    else if (typed[i] === game.passage[i]) game.results[i] = 'correct';
-    else game.results[i] = 'wrong';
+    const oldState = game.results[i];
+    let newState;
+
+    if (typed[i] === undefined) {
+      newState = 'pending';
+    } else if (typed[i] === game.passage[i]) {
+      newState = 'correct';
+    } else {
+      newState = 'wrong';
+    }
+
+    if (newState === 'wrong' || oldState !== 'wrong') {
+      game.errorCounter++;
+    }
+
+    game.results[i] = newState;
   }
 }
 
 function updateAccuracy() {
-  const correctChars = game.results.filter((result) => result === 'correct').length;
-  game.accuracy = (correctChars / game.results.length) * 100;
+  const correct = game.results.filter((r) => r === 'correct').length;
+  const wrong = game.results.filter((r) => r === 'wrong').length;
+  const total = correct + wrong;
+
+  // prevent division by zero
+  game.accuracy = total === 0 ? 100 : (correct / total) * 100;
   accuracy.textContent = `${game.accuracy.toFixed(2)}%`;
 }
 
 // output results to the DOM
 function updateHighlighting() {
   game.results.forEach((result, index) => {
-    passage.children[index].classList.remove('text-green-700', 'text-red-700', 'text-slate-300');
+    passage.children[index].classList.remove(
+      'text-green-500',
+      'text-red-500',
+      'underline',
+      'decoration-3',
+      'text-neutral-400',
+      'bg-white',
+      'opacity-20',
+      'underline-offset-3',
+    );
+
+    game.currentChar = game.results.indexOf('pending');
+
+    passage.children[game.currentChar].classList.add('bg-white', 'opacity-20');
 
     if (result === 'correct') {
-      passage.children[index].classList.add('text-green-700');
+      passage.children[index].classList.add('text-green-500');
     } else if (result === 'pending') {
-      passage.children[index].classList.add('text-slate-300');
+      passage.children[index].classList.add('text-neutral-400');
     } else {
-      passage.children[index].classList.add('text-red-700');
+      passage.children[index].classList.add('text-red-500', 'underline', 'decoration-3', 'underline-offset-6');
     }
   });
+}
+
+function calculateWordsPerMinute() {
+  if (game.mode === 'passage') {
+    game.wordsPerMinute = Math.round(game.words / 60);
+  } else {
+    game.wordsPerMinute = game.words;
+  }
+
+  wpm.textContent = game.wordsPerMinute;
+}
+
+function handleKeydown(e) {
+  if (blockedKeys.includes(e.key)) {
+    e.preventDefault();
+  }
+
+  // count all regular typed chars
+  if (e.key.length === 1) {
+    game.typed++;
+  }
+}
+
+function endGame() {
+  clearInterval(timerId);
+  testComplete.classList.remove('hidden');
+  finalWpm.textContent = game.wordsPerMinute;
+  finalAccuracy.textContent = `${game.accuracy.toFixed(2)}%`;
+
+  const correctChars = game.results.filter((result) => result === 'correct').length;
+  const wrongChars = game.results.filter((result) => result === 'wrong').length;
+  finalCorrect.textContent = correctChars;
+  finalErrors.textContent = wrongChars;
+  userInput.removeEventListener('input', handleInput);
+
+  gameContainer.classList.add('hidden');
 }
 
 // select difficulty
@@ -163,6 +267,10 @@ mode.addEventListener('change', (e) => {
 });
 
 resetButton.addEventListener('click', (e) => {
+  resetGame();
+});
+
+restartButton.addEventListener('click', (e) => {
   resetGame();
 });
 
